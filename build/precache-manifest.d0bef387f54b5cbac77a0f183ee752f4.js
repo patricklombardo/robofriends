ff9d99684a1cbb5982a81203de92c1d227501eea
Patrick Lombardo <patrickjohnlombardo@gmail.com>
2019-03-11 15:51:29 -0400

    First repo push to github

---
+++ build/precache-manifest.d0bef387f54b5cbac77a0f183ee752f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1bc3f121bbf33cde73cb",
+    "url": "/static/js/main.60534e2d.chunk.js"
+  },
+  {
+    "revision": "5036984980ebedae88c2",
+    "url": "/static/js/2.85996f6a.chunk.js"
+  },
+  {
+    "revision": "1bc3f121bbf33cde73cb",
+    "url": "/static/css/main.175b03a0.chunk.css"
+  },
+  {
+    "revision": "5036984980ebedae88c2",
+    "url": "/static/css/2.d393512e.chunk.css"
+  },
+  {
+    "revision": "d54f51bd6adb2f0786ec30df6b14baae",
+    "url": "/index.html"
+  }
+];
